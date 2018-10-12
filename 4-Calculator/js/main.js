e3a5a438719177208c793545bdab940df8a2d21c
Jesse Ma <j.masonoma17@gmail.com>
2018-10-12 15:38:10 -0700

    4-Calculator: Display operations and evaluation of arithmetic expression

--- 4-Calculator/js/main.js
+++ 4-Calculator/js/main.js
@@ -1,38 +1,42 @@
 var output_total = ""
 
 function calculator(e) {
+  var display = document.getElementById('display');
   let target = e.target.id
   // Concatenate the numbers as a string
   // Check if target is NaN
-  if (!isNaN(e.target.id)) {
+  if (!isNaN(target)) {
     console.log('It is a number!');
-    output_total += e.target.id
+    output_total += target
+    // Display output
+    display.innerText = output_total;
+  } else if (target === "=") {
+    evaluateTotal(output_total);
   } else {
     // If an operation is detected, add operation as a string
-    
-    switch(e.target.id) {
+    switch(target) {
       case "-":
-        output_total += "-"
+        output_total += " - "
         break;
       case "+":
-        output_total += "+"
+        output_total += " + "
         break;
       case "*":
-        output_total += "*"
+        output_total += " * "
         break;
       case "/":
-        output_total += "/"
-        break;
-      case "=":
-        evaluateTotal(output_total);
+        output_total += " / "
         break;
       default:
         console.log('Operation not found');
     }
+    // Display output
+    display.innerText = output_total;
   }
   console.log(output_total);
 }
 
 function evaluateTotal(total) {
+  display.innerText = eval(total);
   console.log('Your total is ', eval(total));
 }
